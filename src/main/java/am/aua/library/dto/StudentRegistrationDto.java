41f2a84a050d33ef9578c3af216b612b8e23ec1d
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2024-04-10 22:19:49 +0400

    Creating DTO's for registering students and professors

---
+++ src/main/java/am/aua/library/dto/StudentRegistrationDto.java
@@ -0,0 +1,82 @@
+package am.aua.library.dto;
+
+import com.google.gson.annotations.Expose;
+
+import java.util.Objects;
+
+public class StudentRegistrationDto {
+
+    @Expose
+    private String firstName;
+
+    @Expose
+    private String lastName;
+
+    @Expose
+    private String username;
+
+    @Expose
+    private String password;
+
+    public StudentRegistrationDto(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentRegistrationDto that = (StudentRegistrationDto) o;
+        return Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(username, that.username) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, username, password);
+    }
+
+    @Override
+    public String toString() {
+        return "StudentRegistrationDto{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
