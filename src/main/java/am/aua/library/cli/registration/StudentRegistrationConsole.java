c14b2c4faead16b0927c47db5fae9c51989f2fcb
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2024-04-10 22:20:25 +0400

    implementing the registration logic both for students and for professors

--- src/main/java/am/aua/library/cli/registration/StudentRegistrationConsole.java
+++ src/main/java/am/aua/library/cli/registration/StudentRegistrationConsole.java
@@ -1,6 +1,9 @@
 package am.aua.library.cli.registration;
 
 import am.aua.library.cli.Console;
+import am.aua.library.database.Database;
+import am.aua.library.database.DatabaseException;
+import am.aua.library.dto.StudentRegistrationDto;
 
 /**
  * The StudentRegistrationConsole class represents the console interface for student registration.
@@ -11,8 +14,22 @@ public class StudentRegistrationConsole extends Console {
      * Runs the student registration console interface.
      * This method is to be implemented to define the specific behavior for student registration flow.
      */
+
+    private static final String jsonFileName = "students.json/";
+
+    private StudentRegistrationDto studentRegistrationDto;
+
+    public StudentRegistrationConsole(StudentRegistrationDto studentRegistrationDto) {
+        this.studentRegistrationDto = studentRegistrationDto;
+    }
+
     @Override
     public void run() {
-        // TODO: Implement student registration flow
+        System.out.println(studentRegistrationDto);
+        try {
+            Database.saveStudent(studentRegistrationDto, jsonFileName);
+        } catch (DatabaseException exception) {
+            System.out.println("Could not register the user with the provided credentials.");
+        }
     }
 }
